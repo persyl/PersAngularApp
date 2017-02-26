be0bbcdbae586e79ead9484c93196189ebd50210
Per Lundkvist <per.lundkvist@di.se>
2017-02-26 13:17:45 +0100

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { Hero } from './hero';
+import {HeroService} from './hero.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [HeroService]
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Pers Angular (2) app works!';
+  heroes: Hero[];
+
+  constructor(private heroService: HeroService){
+    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+  }
 }
